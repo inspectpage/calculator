21a3deb648906f99f2689815888bd2359cece617
Thuan Le <106334856+inspectpage@users.noreply.github.com>
2025-12-06 00:25:03 -0500

    Added add,subtract,multiply,divide functions

---
+++ script.js
@@ -0,0 +1,24 @@
+function add(...numbers){
+    return numbers.reduce((total, currentNumber) =>{
+        return total += currentNumber
+    },0)
+}
+
+function subtract(...numbers){
+    return numbers.reduce((total, currentNumber) =>{
+        return total -= currentNumber
+    })
+    
+}
+
+function multiply(...numbers){
+    return numbers.reduce((total, currentNumber) =>{
+        return total *= currentNumber
+    })
+}
+
+function divide(...numbers){
+    return numbers.reduce((total, currentNumber) =>{
+        return total /= currentNumber
+    })
+}
