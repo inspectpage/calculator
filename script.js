3a221ed0033b5c4de15bf70e98ead7c5edb3ff23
Thuan Le <106334856+inspectpage@users.noreply.github.com>
2025-12-09 15:13:33 -0500

    Added Number,Operator, and result Functionality

--- script.js
+++ script.js
@@ -1,9 +1,39 @@
-let firstNumber;
-let secondNumber;
-let operator;
+let firstNumber = "";
+let secondNumber = "";
+let operator = "";
+
+function display(){
+    let operators = ["+","-","x","/"]
+    let equation = document.querySelector(".equation")
+    
+    if (operators.includes(this.textContent)){ // User clicked operator
+        if (!operator){
+            operator = this.textContent;
+    }
+    else { // User clicked number
+        if (!operator){
+            firstNumber += this.textContent
+        }
+        else {
+            secondNumber += this.textContent
+        }
+    }
+    equation.textContent = `${firstNumber} ${operator} ${secondNumber}`
+}
 
 function operate(firstNumber, secondNumber, operator){
-    
+    firstNumber = Number(firstNumber)
+    secondNumber = Number(secondNumber)
+    switch (operator) {
+        case "+":
+            return firstNumber + secondNumber
+        case "-":
+            return firstNumber - secondNumber
+        case "x":
+            return firstNumber * secondNumber
+        case "/":
+            return firstNumber / secondNumber
+    }
 }
 
 function add(...numbers){
@@ -15,18 +45,39 @@ function add(...numbers){
 function subtract(...numbers){
     return numbers.reduce((total, currentNumber) =>{
         return total -= currentNumber
-    })
+    },0)
     
 }
 
 function multiply(...numbers){
     return numbers.reduce((total, currentNumber) =>{
         return total *= currentNumber
-    })
+    },0)
 }
 
 function divide(...numbers){
     return numbers.reduce((total, currentNumber) =>{
         return total /= currentNumber
-    })
+    },0)
 }
+
+let digits = document.querySelectorAll(".digits")
+
+digits.forEach(function(digit){
+    digit.addEventListener("click", display)
+})
+
+let op = document.querySelectorAll(".operator")
+
+op.forEach(function(operator){
+    operator.addEventListener("click", display)
+})
+
+let result = document.querySelector(".result")
+result.addEventListener("click", () => {
+    let input = document.querySelector(".input")
+    if (firstNumber && secondNumber && operator){
+        answer = operate(firstNumber, secondNumber, operator)
+        input.textContent = answer
+    }
+})
